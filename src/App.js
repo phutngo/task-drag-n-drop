edf1d8017a65fd779b61f7fbdbd2911d6f007bc7
Phu Ngo <ptn123@gmail.com>
2021-08-15 12:58:56 -0500

    working  functional comnponents 2 lists

--- src/App.js
+++ src/App.js
@@ -1,26 +1,113 @@
-import initialData from "./initial-data";
-import Column from "./Column";
-import "@atlaskit/css-reset";
-import { DragDropContext } from "react-beautiful-dnd";
+import { useCallback, useReducer } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { data } from "./data";
+import produce from "immer";
 
-function App() {
-  let state = initialData;
-  const onDragEnd = (results) => {
-    //update the state
-  };
+const dragReducer = produce((draft, action) => {
+  switch (action.type) {
+    case "MOVE":
+      {
+        draft[action.from] = draft[action.from] || [];
+        draft[action.to] = draft[action.to] || [];
+        const [removed] = draft[action.from].splice(action.fromIndex, 1);
+        draft[action.to].splice(action.toIndex, 0, removed);
+      }
+      break;
+    default:
+      break;
+  }
+});
+
+const App = () => {
+  const [state, dispatch] = useReducer(dragReducer, {
+    items: data,
+  });
+
+  const onDragEnd = useCallback((result) => {
+    if (result.reason === "DROP") {
+      if (!result.destination) {
+        return;
+      }
+      dispatch({
+        type: "MOVE",
+        from: result.source.droppableId,
+        to: result.destination.droppableId,
+        fromIndex: result.source.index,
+        toIndex: result.destination.index,
+      });
+    }
+  }, []);
 
   return (
-    <>
-      <DragDropContext  onDragEnd={onDragEnd}>
-        {state.columnOrder.map((columnId) => {
-          const column = state.columns[columnId];
-          const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
-
-          return <Column key={column.id} column={column} tasks={tasks} />;
-        })}
-      </DragDropContext>
-    </>
+    <div>
+      HELLLOOWOOWOWOW TIUTLE
+      <div>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId='items' type='PERSON'>
+            {(provided, snapshot) => {
+              return (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                  style={{ display: "block", position: "static", color: "blue", border: "1px solid black" }}
+                >
+                  {state.items?.map((person, index) => {
+                    return (
+                      <Draggable key={person.id} draggableId={person.id} index={index}>
+                        {(provided, snapshot) => {
+                          return (
+                            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                              <div>
+                                <span>
+                                  {person.name.first} {person.name.last}
+                                </span>
+                              </div>
+                            </div>
+                          );
+                        }}
+                      </Draggable>
+                    );
+                  })}
+                  {provided.placeholder}
+                </div>
+              );
+            }}
+          </Droppable>
+          <div style={{ height: "100px" }}>SPACESSSS</div>
+          <Droppable droppableId='items2' type='PERSON'>
+            {(provided, snapshot) => {
+              return (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                  style={{ display: "block", position: "static", color: "red", border: "1px solid orange" }}
+                >
+                  {state.items2?.map((person, index) => {
+                    return (
+                      <Draggable key={person.id} draggableId={person.id} index={index}>
+                        {(provided, snapshot) => {
+                          return (
+                            <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                              <div>
+                                <span>
+                                  {person.name.first} {person.name.last}
+                                </span>
+                              </div>
+                            </div>
+                          );
+                        }}
+                      </Draggable>
+                    );
+                  })}
+                  {provided.placeholder}
+                </div>
+              );
+            }}
+          </Droppable>
+        </DragDropContext>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
