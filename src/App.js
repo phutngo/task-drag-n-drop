ef3113bea963a80dca5ac7eda57583a6c14d4959
Phu Ngo <ptn123@gmail.com>
2021-08-15 11:04:02 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import initialData from "./initial-data";
+import Column from "./Column";
 
 function App() {
+
+  let state = initialData;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {state.columnOrder.map( (columnId) => {
+         const column = state.columns[columnId];
+         const tasks = column.taskIds.map(taskId => state.tasks[taskId]);
+
+         return <Column key={column.id} column={column} tasks={tasks} />;
+      })
+
+
+      }
+      
+
+    </>
   );
 }
 
---
+++ src/Column.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import Task from "./Task";
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+`;
+const Title = styled.h3`
+  padding: 8px;
+`;
+const TaskList = styled.div`
+  padding: 8px;
+`;
+
+const Column = (props) => {
+  return (
+    <div>
+      <Container>
+        <Title>{props.column.title}</Title>
+        <TaskList>
+          {props.tasks.map((task) => (
+            <Task key={task.id} task={task} />
+          ))}
+        </TaskList>
+      </Container>
+    </div>
+  );
+};
+
+export default Column;
